e373a5ecb6b9c9b89440bb44317141efdbef4159
M.O Akindayo <matthewakinse@gmail.com>
2021-08-15 04:44:57 +0300

    Fix db connect bug

--- app/models/database.php
+++ app/models/database.php
@@ -8,16 +8,16 @@ class database{
 
   public function __construct($f3){
     $this->db = new \DB\SQL(
-      'mysql:host=' . $this->f3->DATABASE_ADDRESS 
-    . ';port=' . $this->f3->DATABASE_PORT 
-    . ';dbname=' . $this->f3->DATABASE_NAME, 
-    $this->f3->DATABASE_USER, 
-    $this->f3->DATABASE_PWD, 
+      'mysql:host=' . $f3->DATABASE_ADDRESS 
+    . ';port=' . $f3->DATABASE_PORT 
+    . ';dbname=' . $f3->DATABASE_NAME, 
+    $f3->DATABASE_USER, 
+    $f3->DATABASE_PWD, 
     array(\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES LATIN1;'));
     $this->f3 = $f3;
     $this->crypteri = new twoWayEncrypt($f3->HASH_KEY);
   }
-  
+
   public function getCurrentdate(){
     return date('Y-m-d H:i:s');
   }
